6268aa7b52a84fd94571cc5970956d9901bbfa02
machengyu <chengyuma>
2020-05-22 13:37:46 +0800

    second commit

---
+++ ShortVideo/app/src/main/java/com/example/shortvideo/PagerAdapter.java
@@ -0,0 +1,96 @@
+package com.example.shortvideo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PagerAdapter extends RecyclerView.Adapter<PagerAdapter.VideoViewHolder> {
+
+    private static final String TAG = "PagerAdapter";
+
+    private List<VideoInfo> videoInfoList;
+    private int mNumberItems;
+    private final ItemAdapter.ListItemClickListener mOnClickListener;
+    private static int viewHolderCount;
+
+
+    public PagerAdapter(ItemAdapter.ListItemClickListener listener) {
+        mOnClickListener = listener;
+        viewHolderCount = 0;
+        mNumberItems = 0;
+    }
+
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.pager_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+
+        VideoViewHolder viewHolder = new VideoViewHolder(view);
+
+        viewHolderCount++;
+        Log.i(TAG, "onCreateView:" + videoInfoList.toString());
+        Log.i(TAG, "onCreateView");
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
+        holder.bind(position);
+        Log.i(TAG, "onBindViewHolder  " + position + "    " + mNumberItems);
+        VideoInfo videoInfo = videoInfoList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    public void setData(List<VideoInfo> videoInfoList1) {
+        mNumberItems = videoInfoList1.size();
+        videoInfoList = videoInfoList1;
+        notifyDataSetChanged();
+        Log.i(TAG, videoInfoList.toString());
+        Log.i(TAG, "setData");
+    }
+
+    public class VideoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public VideoView videoView;
+
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            videoView = itemView.findViewById(R.id.vp_videoView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(int position) {
+            VideoInfo videoInfo = videoInfoList.get(position);
+            videoView.setVideoURI(Uri.parse(videoInfo.feedurl));
+            videoView.start();
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            if (mOnClickListener != null) {
+                mOnClickListener.onListItemClick(clickedPosition);
+            }
+        }
+    }
+}
--- ShortVideo/app/src/main/java/com/example/shortvideo/view_pager2.java
+++ ShortVideo/app/src/main/java/com/example/shortvideo/view_pager2.java
@@ -2,26 +2,80 @@ package com.example.shortvideo;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Adapter;
 
-public class view_pager2 extends AppCompatActivity {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class view_pager2 extends AppCompatActivity implements ItemAdapter.ListItemClickListener {
+
+    private static String TAG = "ViewPager2";
+    private static List<VideoInfo> videoInfoList;
+    private ViewPager2 viewPager2;
+    private PagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager2);
 
+        getVideoInfo();
+
         ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
         actionBar.hide();
 
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
+        viewPager2 = findViewById(R.id.viewPager2);
+        mAdapter = new PagerAdapter(this);
+        viewPager2.setAdapter(mAdapter);
+    }
 
+
+    private void getVideoInfo() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://beiyou.bytedance.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        apiService.getVideoInfo().enqueue(new Callback<List<VideoInfo>>() {
+            @Override
+            public void onResponse(Call<List<VideoInfo>> call, Response<List<VideoInfo>> response) {
+                if (response.isSuccessful())
+                    Log.i(TAG, "successful");
+                if (response.body() != null) {
+                    List<VideoInfo> videoInfoList = response.body();
+                    List<VideoInfo> res = response.body();
+                    mAdapter.setData(videoInfoList);
+                    mAdapter.notifyDataSetChanged();
+                    Log.i(TAG, response.body().toString());
+                    VideoInfo temp = res.get(1);
+                    Log.i(TAG, temp.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<VideoInfo>> call, Throwable t) {
+            }
+        });
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+        ;
     }
 }
-
-
